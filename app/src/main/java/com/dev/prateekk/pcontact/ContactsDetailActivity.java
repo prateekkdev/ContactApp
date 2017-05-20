02bde497fb2eafdf769323215ac932a4e143bad3
Prateek Kesarwani <prateek.kesarwani@olacabs.com>
2017-05-20 23:00:03 +0530

    Added layouts

--- app/src/main/java/com/dev/prateekk/pcontact/ContactsDetailActivity.java
+++ app/src/main/java/com/dev/prateekk/pcontact/ContactsDetailActivity.java
@@ -2,9 +2,10 @@ package com.dev.prateekk.pcontact;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.dev.prateekk.pcontact.databinding.ActivityContactsDetailBinding;
 
 /**
  * Created by prateek.kesarwani on 17/05/17.
@@ -12,11 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ContactsDetailActivity extends AppCompatActivity {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    ActivityContactsDetailBinding binding;
 
-        this.setContentView(R.layout.contacts_detail);
-        // DataBindingUtil.setContentView(this, R.layout.contacts_detail);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_contacts_detail);
     }
 }
